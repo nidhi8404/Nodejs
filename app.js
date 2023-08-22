18b3feb134fe47a66df530bc4a55bb82e6065d2b
nidhi8404 <nidhi2shine@gmail.com>
2023-08-23 00:19:11 +0530

    function sayHello

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ function sayHello(name) {
 sayHello('Nidhi');
 sayHello('Tanvi');
 
-// console.log(window); //global obj 
-/*
+/*console.log(window); //global obj 
+
 PS E:\NIDHI\First-App> node app.js
 Hello Nidhi
 Hello Tanvi 
